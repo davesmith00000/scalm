a44ac4861a786ec5d6416e69813abe7dbf722185
Dave Smith <davesmith.00000@gmail.com>
2021-05-24 21:49:28 +0100

    Experimenting with some layout stuff

--- indigo-tools/src/main/scala/example/Main.scala
+++ indigo-tools/src/main/scala/example/Main.scala
@@ -1,32 +1,27 @@
 package example
 
-import scalm.{Html, Scalm}
+import scalm.{Html, Scalm, Sub, Cmd}
 import scalm.Html._
 import org.scalajs.dom.document
 
 object Main:
-  opaque type Model = Int
+  type Msg          = Unit
+  opaque type Model = Unit
 
-  def main(args: Array[String]): Unit =
-    Scalm.start(document.getElementById("myapp"), init, update, view)
+  def init: (Model, Cmd[Msg]) =
+    ((), Cmd.Empty)
 
-  def init: Model = 0
-
-  def update(msg: Msg, model: Model): Model =
-    msg match
-      case Msg.Increment => model + 1
-      case Msg.Decrement => model - 1
+  def update(msg: Msg, model: Model): (Model, Cmd[Msg]) =
+    (model, Cmd.Empty)
 
   def view(model: Model): Html[Msg] =
-    // body()(
-      div(`class`("container"))(
-        div(`class`("row"))(
-          div(`class`("col bodyText"), style("text-align" -> "right"))(button(onClick(Msg.Decrement))(text("-"))),
-          div(`class`("col bodyText"), style("text-align" -> "center"))(text(model.toString)),
-          div(`class`("col bodyText"), style("text-align" -> "left"))(button(onClick(Msg.Increment))(text("+")))
-        )
-      )
-    // )
+    div(`class`("full-width-container"))(
+      TitleBar.view(()),
+      div(`class`("container"))(img("images/bump-example.jpg")())
+    )
 
-enum Msg:
-  case Increment, Decrement
+  def subscriptions(model: Model): Sub[Msg] =
+    Sub.Empty
+
+  def main(args: Array[String]): Unit =
+    Scalm.start(document.getElementById("myapp"), init, update, view, subscriptions)
---
+++ indigo-tools/src/main/scala/example/TitleBar.scala
@@ -0,0 +1,20 @@
+package example
+
+import scalm.{Html, Scalm, Sub, Cmd}
+import scalm.Html._
+import scalm.Component
+
+object TitleBar extends Component:
+
+  type Model = Unit
+
+  type Msg = Unit
+
+  def subscriptions(model: Model): Sub[Msg] =
+    Sub.Empty
+
+  def update(msg: Msg, model: Model): (Model, Cmd[Msg]) =
+    ((), Cmd.Empty)
+
+  def view(model: Model): Html[Msg] =
+    div(`class`("full-width-container"))(text("indigo"))
